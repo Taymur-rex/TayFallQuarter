93cf7e69a84f0b8e2ddf30d6358f19771d0fb3b8
Taymur Dixon <taymur.dixon@hgs.hiddengeniusproject.org>
2025-10-08 18:21:04 -0700

    Heny

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# A route to the home page of line the app 
 @app.route('/')
 def index():
-    return " Hello World"
+    return " Welcome to My FLask App Home Page! "
+
+# A new route to the 'greet' page of the app
+@app.route("/greet/<name>")
+def GreetPage(name):
+    return render_template("index.html", name  = name)
 
 
 if __name__ =='__main__':
